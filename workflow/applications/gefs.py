50561cecc262e021f6d1cbd019d7de5b5be32de6
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-11-19 19:51:53 +0000

    Bring back ice_prod task

--- workflow/applications/gefs.py
+++ workflow/applications/gefs.py
@@ -75,6 +75,9 @@ class GEFSAppConfig(AppConfig):
         if self.do_ocean:
             tasks += ['ocean_prod']
 
+        if self.do_ice:
+            tasks += ['ice_prod']
+
         if self.do_wave:
             tasks += ['wavepostsbs']
             if self.do_wave_bnd:
