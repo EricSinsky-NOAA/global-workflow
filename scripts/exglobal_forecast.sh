90e0647c89fab7e1ac704fcc86570a12dfe4820b
Eric.Sinsky <eric.sinsky@noaa.gov>
2025-06-18 18:21:00 +0000

    Remove wave IC conversion as this will be offline

--- scripts/exglobal_forecast.sh
+++ scripts/exglobal_forecast.sh
@@ -101,7 +101,7 @@ ICETIM=${DELTIM}
 CPL_SLOW=${CPL_SLOW:-${OCNTIM}}
 CPL_FAST=${CPL_FAST:-${ICETIM}}
 
-python ${USHgfs}/ww3_ic_convert.py
+#python ${USHgfs}/ww3_ic_convert.py
 
 echo "MAIN: Loading common variables before determination of run type"
 common_predet
